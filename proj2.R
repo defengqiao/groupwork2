c4ac0fb11c9962b78613ee1971be8ac756f9626e
Defeng Qiao <qiaodefeng@foxmail.com>
2022-10-12 11:42:59 +0100

    fix bugs for fcard1

--- proj2.R
+++ proj2.R
@@ -2,6 +2,7 @@
 #https://github.com/defengqiao/groupwork2.git
 #Contributions
 
+rm(list = ls())
 
 
 #find whether prisoner finds his card and times he tries no matter whether he finds with strategy1
@@ -12,13 +13,11 @@ fcard1 = function(n, k) {
   ncard = sample(1:(2 * n), 2 * n)   #ncard[i] is card number in ith box
   i = k                         #starts at the box with their number on it
   times = 1
-  loop = 1
-  while (k != ncard[i] & loop) {
+  while (k != ncard[i]) {   #he can find his card at the end of the loop
     i = ncard[i]
     times = times + 1
-    loop = (k != ncard[i]) #loop=0 when a loop occurs
   }
-  return(c(loop, times))
+  return(times)             #times he try = length of the loop
 }
 
 fcard2 = function(n, k) {
@@ -26,19 +25,20 @@ fcard2 = function(n, k) {
   startr = sample(1:(2 * n), 1)         #starting from a randomly selected box
   i = startr
   times = 1
-  loop = 1
-  while (k != ncard[i] & loop) {
+  loop = 0
+  while (k != ncard[i] & !loop) {
+    loop = (startr == ncard[i]) #loop=1 when a loop occurs and k is not in the loop   
     i = ncard[i]
     times = times + 1
-    loop = (startr != ncard[i]) #loop=0 when a loop occurs
   }
-  return(c(loop, times))
+  if (loop==1) times= 2*n+1     #it means he can never find his number
+  return(times)
 }
 
 fcard3 = function(n, k) {
   rand = sample(1:(2 * n), 2 * n)   # open n boxes at random, rand[i] means the card number in ith time
   times = match(k, rand)
-  return(c(1, times))
+  return(times)
 }
 #function Pone estimates the probability of a single prisoner succeeding in finding their number
 #arguments: n        2*n is the number of prisoners;
@@ -57,12 +57,11 @@ Pone = function(n, k, strategy, nreps = 10000) {
   }
   wint = 0   #times of success
   for (i in 1:nreps) {
-    result = fcard(n, k)
-    if ((result[1] == 1) & (result[2] <= n)) {
+    if (fcard(n, k) <= n) {  #if he can find his number within n times
       wint = wint + 1
     }
   }
   P = wint / nreps
   return(P)
 }
-print(Pone(50, 1, 3))
+print(Pone(50, 1, 2))
